2ba33106d719ecc56c273bc22ff8ffd800c0dbb5
Neele Hass <neele.hass@student.kit.edu>
2025-02-05 14:29:50 +0100

    add rep_age (reproduktion age als variable)

--- scripts/new_Redkite_eco_constrains/new_Redkite_eco_para_setting.R
+++ scripts/new_Redkite_eco_constrains/new_Redkite_eco_para_setting.R
@@ -202,9 +202,9 @@ coords_juv <- coords_nest[sample(1:nrow(coords_nest), min(inital_new_born, nrow(
 for (i in 1:nrow(coords_juv)) {
   # Add the corresponding number of new borns to each nest
   kites[coords_juv[i,1], coords_juv[i,2], 1, "juv"] <-  TRUE
-  kites[coords_juv[i,1], coords_juv[i,2], 1, "age_lonely"] <-  1 # new kites age
+  kites[coords_juv[i,1], coords_juv[i,2], 1, "age_lonely"] <-  smaple(1:rep_age-1, 1) # new kites age, between new born and almost adult
 
-  #add adult abundance per nest
+  #add juv abundance per nest
   kites[coords_juv[i,1], coords_juv[i,2], 1, "abundance"] <- 
     kites[coords_juv[i,1], coords_juv[i,2], 1, "abundance"] + 1
 
